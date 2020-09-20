ce39bc5911706c49cc57efbf902afaff90cfe5ef
JC <johnchristopherllaguno1@gmail.com>
2020-09-20 23:03:05 +0800

    gravatar & legacy 5.2 extra features completed

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -67,7 +67,7 @@
             <!-- Comment -->
             <div class="media">
                 <a class="pull-left" href="#">
-                    <img width="64" height="64" class="media-object" src="{{$comment->photo}}" alt="">
+                    <img width="64" height="64" class="media-object" src="{{Auth::user()->gravatar}}" alt="">
                 </a>
                 <div class="media-body">
                     <h4 class="media-heading">{{$comment->author}}
